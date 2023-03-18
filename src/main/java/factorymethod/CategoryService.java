80c8135d74a787e155be6015c1ddc742ab2b04fd
thalesAlves758 <thalesalves758@gmail.com>
2023-03-18 15:50:14 -0300

    feat: implements all service functions

--- src/main/java/factorymethod/CategoryService.java
+++ src/main/java/factorymethod/CategoryService.java
@@ -6,22 +6,37 @@ import java.util.ArrayList;
 
 public class CategoryService implements IService<Category> {
     public ArrayList<Category> getAll() {
-        return null;
+        return Category.categories;
     }
 
     public Category getById(int id) {
-        return null;
+        return Category.categories.stream().filter(category -> category.getId() == id).findFirst().orElseGet(null);
     }
 
     public void create(Category object) {
-
+        object.setId(Category.categories.size() + 1);
+        Category.categories.add(object);
     }
 
-    public void update(Category object) {
+    public void update(Category object) throws Exception {
+        Category foundCategory = this.getById(object.getId());
 
+        if(foundCategory == null) {
+            throw new Exception("Could not find this category");
+        }
+
+        int index = Category.categories.indexOf(foundCategory);
+
+        Category.categories.set(index, object);
     }
 
-    public void delete(Category object) {
+    public void delete(Category object) throws Exception {
+        Category foundCategory = this.getById(object.getId());
 
+        if(foundCategory == null) {
+            throw new Exception("Could not find this category");
+        }
+
+        Category.categories.remove(foundCategory);
     }
 }
