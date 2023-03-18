b3e7e8638f5d072f7931e548c093161b7961b185
thalesAlves758 <thalesalves758@gmail.com>
2023-03-18 09:20:30 -0300

    feat: create Rating service base structure

---
+++ src/main/java/factorymethod/RatingService.java
@@ -0,0 +1,27 @@
+package factorymethod;
+
+import entity.Rating;
+
+import java.util.ArrayList;
+
+public class RatingService implements IService<Rating> {
+    public ArrayList<Rating> getAll() {
+        return null;
+    }
+
+    public Rating getById() {
+        return null;
+    }
+
+    public void create(Rating object) {
+
+    }
+
+    public void update(Rating object) {
+
+    }
+
+    public void delete(Rating object) {
+
+    }
+}
