c8e2776db497e1ab24358457bf645b894840bf51
thalesAlves758 <thalesalves758@gmail.com>
2023-03-18 09:08:37 -0300

    feat: add id property and getter and setter

--- src/main/java/entity/Category.java
+++ src/main/java/entity/Category.java
@@ -1,7 +1,16 @@
 package entity;
 
 public class Category {
+    private int id;
     private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
