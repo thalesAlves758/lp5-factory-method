09e8c939c2689c7c47bcf81417f35a4db4927ff1
thalesAlves758 <thalesalves758@gmail.com>
2023-03-18 09:21:11 -0300

    feat: create WishList service base structure

---
+++ src/main/java/factorymethod/WishListService.java
@@ -0,0 +1,27 @@
+package factorymethod;
+
+import entity.WishList;
+
+import java.util.ArrayList;
+
+public class WishListService implements IService<WishList> {
+    public ArrayList<WishList> getAll() {
+        return null;
+    }
+
+    public WishList getById() {
+        return null;
+    }
+
+    public void create(WishList object) {
+
+    }
+
+    public void update(WishList object) {
+
+    }
+
+    public void delete(WishList object) {
+
+    }
+}
