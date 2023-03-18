c8f1b582a1f3bf08512862706117463c1afc67d9
thalesAlves758 <thalesalves758@gmail.com>
2023-03-18 09:18:47 -0300

    feat: create Product Service base structure

---
+++ src/main/java/factorymethod/ProductService.java
@@ -0,0 +1,27 @@
+package factorymethod;
+
+import entity.Product;
+
+import java.util.ArrayList;
+
+public class ProductService implements IService<Product> {
+    public ArrayList<Product> getAll() {
+        return null;
+    }
+
+    public Product getById() {
+        return null;
+    }
+
+    public void create(Product object) {
+
+    }
+
+    public void update(Product object) {
+
+    }
+
+    public void delete(Product object) {
+
+    }
+}
