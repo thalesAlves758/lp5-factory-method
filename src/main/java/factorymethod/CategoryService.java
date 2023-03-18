13c63bf473e3e19351763a8b66bb871baf7ece08
thalesAlves758 <thalesalves758@gmail.com>
2023-03-18 09:19:47 -0300

    feat: create Category service base structure

---
+++ src/main/java/factorymethod/CategoryService.java
@@ -0,0 +1,27 @@
+package factorymethod;
+
+import entity.Category;
+
+import java.util.ArrayList;
+
+public class CategoryService implements IService<Category> {
+    public ArrayList<Category> getAll() {
+        return null;
+    }
+
+    public Category getById() {
+        return null;
+    }
+
+    public void create(Category object) {
+
+    }
+
+    public void update(Category object) {
+
+    }
+
+    public void delete(Category object) {
+
+    }
+}
