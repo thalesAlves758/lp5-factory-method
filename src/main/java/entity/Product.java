b5c1290342cae17fbdebb5c517edfefa5877135a
thalesAlves758 <thalesalves758@gmail.com>
2023-03-17 22:30:27 -0300

    init project

---
+++ src/main/java/entity/Product.java
@@ -0,0 +1,22 @@
+package entity;
+
+public class Product {
+    private String name;
+    private Double value;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+}
---
+++ src/main/java/entity/Rating.java
@@ -0,0 +1,22 @@
+package entity;
+
+public class Rating {
+    private Double starValue;
+    private String comment;
+
+    public Double getStarValue() {
+        return starValue;
+    }
+
+    public void setStarValue(Double starValue) {
+        this.starValue = starValue;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+}
