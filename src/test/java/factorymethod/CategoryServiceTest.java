cfdfb080beba11d9b2972dd0821856893686ce0d
thalesAlves758 <thalesalves758@gmail.com>
2023-03-18 15:51:10 -0300

    test: create CategoryService test

---
+++ src/test/java/factorymethod/CategoryServiceTest.java
@@ -0,0 +1,28 @@
+package factorymethod;
+
+import entity.Category;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class CategoryServiceTest {
+    @BeforeAll
+    void setUp() {
+        Category.categories.add(new Category(1, "Teste"));
+    }
+
+    @Test
+    void shouldReturnCategoryList() {
+        IService categoryService = ServiceFactory.getService("CategoryService");
+
+        ArrayList<Category> categories = categoryService.getAll();
+
+        assertTrue(categories instanceof ArrayList);
+        assertEquals(categories.stream().findFirst().get(), Category.categories.stream().findFirst().get());
+    }
+}
